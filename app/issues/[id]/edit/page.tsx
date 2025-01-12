3fd2ea1cedfd2f8066b4474054a807354d28dde0
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-01-12 17:18:27 +0100

    Update issues

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import IssueForm from '../../_components/IssueForm'
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-interface Props{
-    params: {id: string}
-}
-const EditIssuePage = async({params}: Props) => {
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    })
-    
-    if(!issue) notFound();
+import React from "react";
+import IssueForm from "../../_components/IssueForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
 
-  return (
-    <IssueForm issue={issue} /> 
-  )
+interface Props {
+  params: { id: string };
 }
 
-export default EditIssuePage
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
