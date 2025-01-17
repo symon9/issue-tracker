c799e8b2f85f78e305b8f4b184a623e5f87cd590
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-01-17 21:08:15 +0100

    fix params bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Pagination from "./components/Pagination";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
+  const page = await searchParams;
+
   return (
     <Pagination
       itemCount={100}
       pageSize={10}
-      currentPage={parseInt(searchParams.page)}
+      currentPage={parseInt(page.page)}
     />
   );
 }
